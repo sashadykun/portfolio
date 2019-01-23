9a28a878c9144e94529e2a51bf49989bb7db6f1d
Oleksandr Dykun <sashaluckyman@gmail.com>
2019-01-22 22:35:00 -0800

    about

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,25 @@
-// $(document).ready( function(){
-//     $("#messageSubmit").click( sendMessageToServer);
-// })
 
-// function sendMessageToServer(){
-//     return { email: "sashaluckyman@gmail.com"}
-// }
+$(document).ready( assignClickHandler);
+
+function assignClickHandler(){
+    $("#submit").on('click', sendMessageToServer);    
+}
+
+function sendMessageToServer(){
+
+ 
+   const message = {
+       name:  $("#name").val(),
+       email:   $('#email').val(),
+       message: $('#message').val()
+   }
+   const API = "localhost:5000/send"
+   fetch(API, {
+       method: getComputedStyle,
+       body: JSON.stringify(message),
+       header: {
+           'Contact-Type': 'application/json',
+           'Accept': 'application/json'
+       }
+   }).then(response => response.json()).then(()=> console.log('data sent'))
+}
